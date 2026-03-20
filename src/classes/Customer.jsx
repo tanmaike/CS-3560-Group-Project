b958d8608b91a70921ef4e8fd778a8a46d570a4e
Roven James Rivera <105902511+rovenrivera@users.noreply.github.com>
2026-03-19 23:57:42 -0700

    Added comments for logic in functions

--- src/classes/Customer.jsx
+++ src/classes/Customer.jsx
@@ -8,31 +8,65 @@ function Customer() {
   const [paymentsDue, setPaymentsDue] = useState(-1.0);
   const [vehicles, setVehicles] = useState([-1]);
 
-  const updateProfile = () => {
+  const createProfile = () => {
+    /*
+    ========================== TO-DO ==========================
+    Implement function to set customer name, ID, insurance, payment,
+    and vehicles upon profile creation
+    */
     return
   }
 
   const updateInfo = () => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to set customer name, ID, insurance, payment,
+    and vehicles upon updating profile information
+    */
     return
   }
 
   const linkVehicle = () => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to add vehicle to customer profile's
+    vehicles list
+    */
     return
   }
 
   const viewVehicleStatus = () => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to return vehicle status on customer side
+    (quotes, maintenance updates, appointments)
+    */
     return
   }
 
   const linkInsurance = () => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to add/change insurance policy ID of customer profile
+    */
     return
   }
 
   const recordIssueRequest = () => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function that sends vehicle issue request made on
+    customer side to mechanic side 
+    */
     return
   }
 
   const makePayment = (money) => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to send payment through, upon sending payment,
+    remove the payment from "paymentsDue"
+    */
     return .0
   }
 
--- src/classes/Manager.jsx
+++ src/classes/Manager.jsx
@@ -6,18 +6,37 @@ function Manager() {
   const [managerID, setManagerID] = -1;
 
   const terminateJob = (jobID) => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to remove job from mechanic's "assignedJobs"
+    */
     return
   }
 
   const recordQuote = (quoteID) => {
+    /* 
+    ========================== TO-dDO ==========================
+    Implement function where upon completion of mechanic assigned job, 
+    create quote for payment that will go through to customer side, adding to "paymentsDue"
+    */
     return
   }
 
   const updateQuote = (quoteID) => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function where upon receieving vehicle diagnosis and
+    cost from mechanic, update quote for payment accordingly for
+    customer side
+    */
     return
   }
 
   const assignMechanics = (mechanicID) => {
+    /* 
+    ========================== TO-DO ==========================
+    Implement function to add a job to mechanic's "assignedJobs"
+    */
     return
   }
 
